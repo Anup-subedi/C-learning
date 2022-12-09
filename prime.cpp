9ae3b434c3c0a44511a2d7d830464f711283ae65
Anup033 <anupsubedi315@gmail.com>
2022-12-09 21:21:52 +0545

    prime_1

--- prime.cpp
+++ prime.cpp
@@ -6,15 +6,18 @@ using namespace std;
 int main()
 {
     cout<<"\n-----------program to check whether the number is prime or not-----------\n\n";
+    do{
+       system("cls");
     int count=0, n,i; //declaring variables
     cout<<"Enter a number:";
     cin>>n; //takes the input
 
-    if(n==1 || n==0) //checking condition for 1 and 0 as they don't belong to prime or composite.
-    cout<< n << " is neither prime nor composite.\n";
+    if (n==1 || n==0) //checking condition for 1 and 0 as they don't belong to prime or composite.
+    {cout<< n << " is neither prime nor composite.\n";}
+
     else
   {
-   for (i=2; i<=n/2; i++) //itirating upto n/2. Since, if the number is not devisible until its half then it isn't devisible. 
+   for (i=1; i<=n; i++) //itirating upto n/2. Since, if the number is not devisible until its half then it isn't devisible. 
     {
       
       if (n%i==0)
@@ -24,10 +27,16 @@ int main()
       {break;}
     }
 
-  }  
+ 
    if (count==2)
    {
    cout<< n << " is a prime number!\n";}
    else
    cout<< n << " is a composite number!\n";
+  }
+ cout << "\nDo you want to continue? (y/n): \n";
+    } while (getchar() == 121);
+    cout << "\nBye\n";
+    system("pause");
+    return 0;
 }
