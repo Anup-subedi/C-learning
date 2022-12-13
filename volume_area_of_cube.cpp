1476403f067effa8e40d6626d7eb8c4b2c188ab1
Anup Subedi <106690260+Anup033@users.noreply.github.com>
2022-12-13 19:37:08 +0545

    documented

--- volume_area_of_cube.cpp
+++ volume_area_of_cube.cpp
@@ -1,22 +1,27 @@
 #include<iostream>
 //using namespace std;
- 
- class box{
-private:
-int l, b, h;
 
-public:
-void setvalues()
+//Note: I used std::cin and std::cout to fix the vs studio code issue with syntax.
+ 
+//This is class which contains volume and surface area.
+class box{ 
+ 
+ private: //these variables can't be accesssed by any objects 
+ int l, b, h; //variables declaration
+
+  
+ public: //this function can be accessed by all the objects.
+ void setvalues()
 {
-    std::cout<< "Enter Dimensions(Length|Breadth|Height)\n";
+    std::cout<< "Enter Dimensions(Length|Breadth|Height)\n"; //asking value to the user
     std::cin >> l >> b >> h;
 
 }
-int getvolume()
+int getvolume() //it returns the volume by taking dimensions input from the user.
 {
   return l*b*h;  
 }
-void printtsa()
+void printtsa() //it directly prints Total surface area when it is called in the main function.
 {
   std::cout << "Total Surface Area:" << 2*(l*b + b*h + l*h);
 }
@@ -26,12 +31,13 @@ void printtsa()
  int main()
  {
 
-  box box1, box2;
+  box box1, box2; //declaring objects of the class box.
 
-   box1.setvalues();
+    //calling the functions
+   box1.setvalues(); 
    box1.printtsa();
 
-
+ //printing the returned value by getvolume() function
    std::cout << "Volume of box-1:";
    box1.getvolume();
    std::cout << "\n";
