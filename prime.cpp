56378c1506d7c0cb9ae3dfebc9e4729c645961d5
Anup Subedi <106690260+Anup033@users.noreply.github.com>
2022-12-16 22:29:46 +0545

    optimized-code

--- prime.cpp
+++ prime.cpp
@@ -3,8 +3,33 @@ using namespace std;
 
 int main()
 {
-    cout<<"Hello world!! \n";
-    cout<<"This prints the new line \n";
-
+    int i, n, count=0;
+    cout << "Enter any positive number: ";
+    cin >> n;
+    
+    if (n==1 || n=0)
+        cout << n <<" is neither composite nor prime";
+    if (n==2)
+        cout << n <<" is prime";
+    
+   for(i=1; i<=n/2; i++)
+   {
+       if (n%i==0)
+       {count++;}
+       
+       if (count==2) 
+       {break;}
+   }
+    
+    cout << i << "- times the iterations are done"; //checking the number of iterations
+ if (count==1)
+ { 
+     cout << n << "is prime number";
+ }   
+    else
+ {
+        cout << n << "is a composite number";
+ }
+    
 return 0;
 }
